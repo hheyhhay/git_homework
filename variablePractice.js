3e8585d1354d95d7fadf2a81368215a83a082913
Hayley Dixon <hdixon37@gmail.com>
2021-02-22 15:42:58 -0700

    Varibles added

---
+++ variablePractice.js
@@ -0,0 +1,6 @@
+var statement = 'Learning how to use Git';
+var typesOfCommand = ['git init', 'git add', 'git commit', 'git status', 'git add', 'git diff'];
+var learningComplete = false;
+var numberOfCommand = 6;
+var homeworkComplete = true;
+var name = 'Hayley Dixon';
